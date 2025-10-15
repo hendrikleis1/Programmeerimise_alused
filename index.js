22d591aac5f127c0319b6fdd2e3b7ece6af54ce2
HendrikLeis <hendrik.leis@voco.ee>
2025-10-15 19:56:55 +0300

    Programmeerimise aluste esimene fail.

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("Hello");
+// console.log("I like pizza");
+
+// window.alert(`This is an alert!`);
+// window.alert(`I like pizza!`);
+
+// This is a comment
+
+document.getElementById("myH1").textContent = `Hello`;
+
+/* this is 
+    a
+    comment
+*/
+
+let x;
+x=100;
+
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(gpa);
+console.log(`You are ${age} years old.`)
